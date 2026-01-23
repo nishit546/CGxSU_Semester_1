c5cae06283cd849714d6a9c8802c49990d16109f
Kshatriya Samir Singh <120371957+samirsinghkshatriya@users.noreply.github.com>
2026-01-23 12:07:51 -0500

    do it

--- html_css_js(sem_01)/JS_DOM/02.dom/04.Exercise /index.js
+++
@@ -1,9 +0,0 @@
-const countText = document.getElementById("countText");
-const increaseBtn = document.getElementById("increaseBtn");
-
-let count = 0;
-
-increaseBtn.addEventListener("click", () => {
-  count = count + 1; // increase by 1
-  countText.textContent = "Count: " + count;
-});
